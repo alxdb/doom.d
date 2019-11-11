8f04e0284ac58daafaa6bc0f3fb5fb796b31f81d
Alex Davidson Bryan <alxdavbr@gmail.com>
2019-11-11 14:17:25 +0000

    mac sync

--- config.el
+++ config.el
@@ -1,22 +1,26 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 
-(setq doom-theme 'doom-gruvbox
+(setq doom-theme (cond (IS-LINUX 'doom-gruvbox)
+                       (IS-MAC 'doom-city-lights))
       doom-font (font-spec :family (cond (IS-LINUX "Input Mono")
                                          (IS-MAC "SF Mono"))
-                           :size 16)
+                           :size (cond (IS-LINUX 16)
+                                       (IS-MAC 12)))
       doom-big-font (font-spec :family (cond (IS-LINUX "Input Mono")
                                              (IS-MAC "SF Mono"))
-                               :size 32)
+                               :size (cond (IS-LINUX 32)
+                                           (IS-MAC 16)))
       doom-variable-pitch-font (font-spec :family (cond (IS-LINUX "Input Sans")
                                                         (IS-MAC "Helvetica Neue"))
-                                          :size 16))
+                                          :size (cond (IS-LINUX 16)
+                                                      (IS-MAC 12))))
 (setq rainbow-delimiters-max-face-count 9)
-(setq +workspaces-on-switch-project-behavior t)
+;; (setq +workspaces-on-switch-project-behavior t)
 
 (after! projectile
   (setq projectile-project-search-path (cond (IS-LINUX "~/projects")
-                                             (IS-MAC "~/Documents/Projects")))
+                                             (IS-MAC "~/Documents/GitHub")))
   (setq projectile-indexing-method 'hybrid))
 (after! clojure-mode
   (require 'flycheck-clj-kondo))
@@ -24,3 +28,6 @@
 (add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
 (set-popup-rule! "\\*cider-scratch\\*" :ignore t)
 (set-popup-rule! "\\*SQL\\*" :kill nil :ttl nil)
+(add-hook! 'cider-repl-mode-hook #'lispy-mode #'lispyville-mode #'rainbow-delimiters-mode)
+(map! (:map cider-repl-mode-map
+        :g "M-n" 'cider-repl-next-input))
