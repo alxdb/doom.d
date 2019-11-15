f0d2025c400e93621dfc62ec164183849bfaaa4f
Alex Davidson Bryan <alxdavbr@gmail.com>
2019-11-15 12:24:59 +0000

    mac changes

--- config.el
+++ config.el
@@ -1,33 +1,35 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
-
+;; Global Settings
 (setq doom-theme (cond (IS-LINUX 'doom-gruvbox)
-                       (IS-MAC 'doom-city-lights))
+                       (IS-MAC 'doom-one))
       doom-font (font-spec :family (cond (IS-LINUX "Input Mono")
                                          (IS-MAC "SF Mono"))
                            :size (cond (IS-LINUX 16)
-                                       (IS-MAC 12)))
-      doom-big-font (font-spec :family (cond (IS-LINUX "Input Mono")
-                                             (IS-MAC "SF Mono"))
-                               :size (cond (IS-LINUX 32)
-                                           (IS-MAC 16)))
+                                       (IS-MAC 13))
+                           :weight (cond (IS-LINUX 'normal)
+                                         (IS-MAC 'light)))
       doom-variable-pitch-font (font-spec :family (cond (IS-LINUX "Input Sans")
                                                         (IS-MAC "Helvetica Neue"))
                                           :size (cond (IS-LINUX 16)
-                                                      (IS-MAC 12))))
+                                                      (IS-MAC 13))))
 (setq rainbow-delimiters-max-face-count 9)
-;; (setq +workspaces-on-switch-project-behavior t)
+(setq +workspaces-on-switch-project-behavior t)
 
+;; Projectile
 (after! projectile
   (setq projectile-project-search-path (cond (IS-LINUX "~/projects")
                                              (IS-MAC "~/Documents/GitHub")))
   (setq projectile-indexing-method 'hybrid))
+
+;; Clojure
 (after! clojure-mode
   (require 'flycheck-clj-kondo))
-
-(add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
-(set-popup-rule! "\\*cider-scratch\\*" :ignore t)
-(set-popup-rule! "\\*SQL\\*" :kill nil :ttl nil)
-(add-hook! 'cider-repl-mode-hook #'lispy-mode #'lispyville-mode #'rainbow-delimiters-mode)
+(add-hook! 'cider-repl-mode-hook #'lispy-mode #'rainbow-delimiters-mode)
+(add-hook! 'clojure-mode-hook #'prettify-symbols-mode)
 (map! (:map cider-repl-mode-map
         :g "M-n" 'cider-repl-next-input))
+
+;; Popup Windows
+(set-popup-rule! "\\*cider-scratch\\*" :ignore t)
+(set-popup-rule! "\\*SQL\\*" :kill nil :ttl nil)
--- init.el
+++ init.el
@@ -78,12 +78,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("1728dfd9560bff76a7dc6c3f61e9f4d3e6ef9d017a83a841c117bd9bebe18613" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" default)))
+    ("4e78818bf9e231d3f0d03798886298837137bbbde0f3daf343ec2ec85dc90d91" "1728dfd9560bff76a7dc6c3f61e9f4d3e6ef9d017a83a841c117bd9bebe18613" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "071f5702a5445970105be9456a48423a87b8b9cfa4b1f76d15699b29123fb7d8" default)))
  '(org-agenda-files (quote ("~/Documents/yapfaqs.org")))
  '(projectile-indexing-method (quote hybrid))
  '(safe-local-variable-values
    (quote
-    ((helm-make-arguments . "-j7")
+    ((cider-clojure-cli-global-options . "-A:dev")
+     (helm-make-arguments . "-j7")
      (projectile-project-configure-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..")
      (projectile-project-run-cmd . "./run.sh")
      (projectile-project-name . "adbGL")
