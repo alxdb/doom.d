42a8af893fa704417e34f4684740b94958ca0093
Alexander Davidson Bryan <alxdb@pm.me>
2021-03-06 17:34:29 +0000

    use default monospace font

--- config.el
+++ config.el
@@ -3,7 +3,7 @@
 (setq user-full-name "Alexander Davidson Bryan"
       user-mail-address "alxdb@pm.me")
 
-(setq doom-font (font-spec :family "Iosevka" :size 22)
+(setq doom-font (font-spec :family "monospace" :size 22)
       doom-variable-pitch-font (font-spec :family "sans" :size 22))
 
 (setq doom-theme 'doom-nord)
