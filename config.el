ffdf943609d541fcdaa676a63e69f471b06b5461
Alexander Davidson Bryan <alxdb@pm.me>
2021-04-20 22:18:40 +0100

    proper commenting (satisfy formatter)

--- config.el
+++ config.el
@@ -24,7 +24,7 @@
 (setq! doom-themes-treemacs-enable-variable-pitch nil)
 (setq! doom-themes-treemacs-theme 'doom-colors)
 
-; https://github.com/emacs-lsp/lsp-treemacs/issues/89
+;; https://github.com/emacs-lsp/lsp-treemacs/issues/89
 (with-eval-after-load 'lsp-treemacs
   (load-library "doom-themes-ext-treemacs"))
 
