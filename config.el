023c812f7ded28f930efb29841639884a90c1071
Alexander Davidson Bryan <alxdb@pm.me>
2022-03-17 23:37:30 +0000

    tweaks

--- config.el
+++ config.el
@@ -1,20 +1,25 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq user-full-name "Alexander Davidson Bryan"
-      user-mail-address "alxdb@pm.me")
+(setq! user-full-name "Alexander Davidson Bryan"
+       user-mail-address "alxdb@pm.me")
 
 (setq doom-font (font-spec :family "Iosevka" :size 13)
-      doom-variable-pitch-font (font-spec :family "Input Serif Condensed" :size 15))
+      doom-variable-pitch-font (font-spec :family "Iosevka" :size 13))
 
 (setq doom-theme 'doom-one)
 
 (custom-set-faces!
-  '(default :foreground "#cfd7e5"))
+  '(default
+     :foreground "#cfd7e5"
+     :background "#20232b"))
 
-(setq display-line-numbers-type t)
+(setq! display-line-numbers-type t)
 
-(setq org-directory "~/Documents/org/")
+(setq! org-directory "~/Documents/org/"
+       projectile-project-search-path "~/Projects")
 
-(after! evil (evil-set-initial-state 'vterm-mode 'emacs))
+(set-evil-initial-state! 'vterm-mode 'emacs)
 
-(setq treemacs-collapse-dirs 10)
+(setq! treemacs-collapse-dirs 10)
+
+(setq! lsp-lens-enable nil)
