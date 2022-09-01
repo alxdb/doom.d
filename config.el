dc7d0621f544f9f4fcd76fd3a3a936d8c78b58a5
Alexander Davidson Bryan <alxdb@pm.me>
2022-09-01 18:40:54 +0100

    reformat

--- config.el
+++ config.el
@@ -3,18 +3,19 @@
 (setq user-full-name "Alexander Davidson Bryan"
       user-mail-address "alxdb@pm.me")
 
-(setq doom-font (font-spec :family "monospace" :size 26)
-      doom-variable-pitch-font (font-spec :family "monospace" :size 26))
-
 (setq doom-theme 'one-dark
+      doom-font (font-spec :family "monospace" :size 26)
+      doom-variable-pitch-font (font-spec :family "monospace" :size 26)
       all-the-icons-scale-factor 1.0
       doom-modeline-height 44)
 
 (setq org-directory "~/org/"
-      projectile-project-search-path '("~/src/"))
+      +workspaces-on-switch-project-behavior t)
 
-(setq +workspaces-on-switch-project-behavior t
-      projectile-indexing-method 'alien)
+(after! projectile
+  (setq projectile-project-search-path '("~/src/")
+        projectile-indexing-method 'alien
+        projectile-enable-cmake-presets t))
 
 (after! evil
   (set-evil-initial-state! '(vterm-mode term-mode) 'emacs)
